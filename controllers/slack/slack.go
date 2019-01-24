941e946d44931459d9a25be31832c0d0ed871d25
Andrew Eggleston <egglestonandrew927@gmail.com>
2019-01-24 03:11:14 -0500

    Debugging message events

--- controllers/slack/slack.go
+++ controllers/slack/slack.go
@@ -25,17 +25,51 @@ func (s *SlackListener) ListenAndResponse() {
 
 	logrus.Info("Listening for Slack messages")
 
+
+
 	for msg := range rtm.IncomingEvents {
+		fmt.Print("Event Received: ")
 		switch ev := msg.Data.(type) {
+		case *slack.HelloEvent:
+			// Ignore hello
+
+		case *slack.ConnectedEvent:
+			fmt.Println("Infos:", ev.Info)
+			fmt.Println("Connection counter:", ev.ConnectionCount)
+
 		case *slack.MessageEvent:
-			if err := s.handleMessageEvent(ev); err != nil {
-				logrus.Error("Failed to handle message: %s", err)
-			}
+			fmt.Printf("Message: %v\n", ev)
+
+		case *slack.PresenceChangeEvent:
+			fmt.Printf("Presence Change: %v\n", ev)
+
+		case *slack.LatencyReport:
+			fmt.Printf("Current latency: %v\n", ev.Value)
+
+		case *slack.RTMError:
+			fmt.Printf("Error: %s\n", ev.Error())
+
+		case *slack.InvalidAuthEvent:
+			fmt.Printf("Invalid credentials")
+			return
 
 		default:
-			logrus.Debugf("Message event: %s", &ev)
+
+			// Ignore other events..
+			// fmt.Printf("Unexpected: %v\n", msg.Data)
 		}
-	}
+
+	//for msg := range rtm.IncomingEvents {
+	//	switch ev := msg.Data.(type) {
+	//	case *slack.MessageEvent:
+	//		if err := s.handleMessageEvent(ev); err != nil {
+	//			logrus.Error("Failed to handle message: %s", err)
+	//		}
+	//
+	//	default:
+	//		logrus.Debugf("Message event: %s", ev)
+	//	}
+	//}
 }
 
 func (s *SlackListener) handleMessageEvent(ev *slack.MessageEvent) error {
