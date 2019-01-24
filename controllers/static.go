01b5893252eec236224ed152a3d4d5dddb64b408
Andrew Eggleston <egglestonandrew927@gmail.com>
2019-01-23 21:07:49 -0500

    Updated static html page

--- controllers/static.go
+++ controllers/static.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/andreweggleston/DeathByDagger/controllers/controllerhelpers"
 	"github.com/andreweggleston/DeathByDagger/inside/version"
+	"github.com/andreweggleston/DeathByDagger/models/player"
 	"github.com/sirupsen/logrus"
 	"html/template"
 	"net/http"
@@ -19,10 +21,19 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var p *player.Player
+	tok, err := controllerhelpers.GetToken(r)
+
+	if err == nil {
+		p = controllerhelpers.GetPlayer(tok)
+	}
+
 	errtempl := mainTempl.Execute(w, map[string]interface{}{
-		"BuildDate": 	version.BuildDate,
-		"GitCommit":	version.GitCommit,
-		"GitBranch":	version.GitBranch,
+		"LoggedIn":  err == nil,
+		"Player":    p,
+		"BuildDate": version.BuildDate,
+		"GitCommit": version.GitCommit,
+		"GitBranch": version.GitBranch,
 		"BuildInfo": fmt.Sprintf("Build using %s on %s (%s %s)", runtime.Version(), version.Hostname, runtime.GOOS, runtime.GOARCH),
 	})
 	if errtempl != nil {
