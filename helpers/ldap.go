b104599f5cd90a8683affee7b1bf20e50c7bd4ab
Andrew Eggleston <egglestonandrew927@gmail.com>
2019-01-30 23:32:20 -0500

    Removed debugging test

--- helpers/ldap.go
+++ helpers/ldap.go
@@ -2,7 +2,6 @@ package helpers
 
 import (
 	"fmt"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/sirupsen/logrus"
 	"gopkg.in/ldap.v3"
 )
@@ -22,7 +21,6 @@ func (l *LDAP) SearchForSlackUID(slackUID string) (*ldap.Entry, error) {
 	}
 
 	for _, entry := range sr.Entries {
-		spew.Dump(entry)
 		fmt.Printf("%s: %v\n", entry.DN, entry.GetAttributeValue("uid"))
 	}
 
